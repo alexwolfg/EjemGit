55d7bd86ffe85123f81f40d871c4327f8e332342
alex <alejandro@telecos>
2018-06-20 10:59:05 +0200

    Versión del día 20 de Junio de 2018. Primera versión del ejecutable Conf_OCB_Interface para el lanzamiento del modo OCB. Funciona correctamente.

---
+++ Conf_OCB_Interface.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+ip link set wlp3s0 down
+
+ip link set wlp2s0 down
+iw dev wlp2s0 interface add ocb0 type ocb
+iw dev ocb0 set type ocb
+ip link set ocb0 down
+iw reg set AA
+iw reg get
+ip link set ocb0 up
+iw dev ocb0 ocb join 5880 10MHZ
+iw dev | iwconfig
+
+ifconfig ocb0 192.168.1.2 netmask 255.255.255.0 
+
+
+
